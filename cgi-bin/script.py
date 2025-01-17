8529c32124938aae51bb90342d908e9358815014
Achraf Ibn Cheikh <aibn-che@e3r9p15.1337.ma>
2025-01-17 15:27:19 +0100

    add some notes

--- cgi-bin/script.py
+++ cgi-bin/script.py
@@ -3,11 +3,11 @@ import os
 thislist = ["My", "First", "Python", "Execution", "With CGI"]
 
 
+# /cgi-bin/hello.py/12345
+path_info = os.environ['PATH_INFO']
+print("Received PATH_INFO:", path_info) # 12345
 
-print("Server: TTS \r\n", end="")
-print("Set-Cookie: prov=f0d02f8c-733c-4d5a-9494-1797b9c45205 \r\n", end="")
-print("Content-Type: text/html       \r\n", end="")
-print("Content-Type: fgfh/fhfgh       \r\n\r\n", end="")
+
 
 # print('Content-Type: ' + os.environ['REQUEST_METHOD']+'\r\n', end="")
 print(" ".join(thislist))
